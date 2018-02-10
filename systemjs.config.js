44cfa80abc030bf4bc7ffd57c4a3593ee9c1af83
ilya1995 <kovalyov.king@yandex.ru>
2018-02-10 20:26:25 +0300

    delete js files

--- systemjs.config.js
+++ systemjs.config.js
@@ -29,11 +29,11 @@
             },
             rxjs: {
                 defaultExtension: 'js'
-            },
-            'angular-in-memory-web-api': {
-                main: './index.js',
-                defaultExtension: 'js'
             }
+            // 'angular-in-memory-web-api': {
+            //     main: './index.js',
+            //     defaultExtension: 'js'
+            // }
         }
     });
 })(this);
